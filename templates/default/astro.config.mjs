2c49dc145bc161ebec95db9c5c493d54b86a120f
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2024-08-22 05:42:51 +0700

    🚧 Update stuff

--- templates/default/astro.config.mjs
+++ templates/default/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown(), tailwind(), mdx()],
+  integrations: [tailwind(), mdx()],
   output: "server",
 });
